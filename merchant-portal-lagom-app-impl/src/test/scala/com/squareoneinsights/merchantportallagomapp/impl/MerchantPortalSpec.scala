d0b8a79463afeb428abc48e8f8e5cacf64c94f70
Tushar <tushar.dhane@squareoneinsights.com>
2022-09-30 08:32:03 +0530

    Implemented lagom testcases for add and updated api

--- merchant-portal-lagom-app-impl/src/test/scala/com/squareoneinsights/merchantportallagomapp/impl/MerchantPortalSpec.scala
+++ merchant-portal-lagom-app-impl/src/test/scala/com/squareoneinsights/merchantportallagomapp/impl/MerchantPortalSpec.scala
@@ -3,6 +3,7 @@ package com.squareoneinsights.merchantportallagomapp.impl
 import com.lightbend.lagom.scaladsl.server.LocalServiceLocator
 import com.lightbend.lagom.scaladsl.testkit.ServiceTest
 import com.squareoneinsights.merchantportallagomapp.api.MerchantportallagomappService
+import com.squareoneinsights.merchantportallagomapp.api.request.MerchantRiskScoreReq
 import com.wix.accord.dsl.Contextualizer
 import org.scalatest.{AsyncWordSpec, Matchers}
 
@@ -38,5 +39,32 @@ class MerchantPortalSpec extends AsyncWordSpec with Matchers {
     }
   }
 
+  "AddOrUpdate Risk Score for MerchantID" should {
+    "say hello" in ServiceTest.withServer(ServiceTest.defaultSetup.withCluster()) { ctx =>
+      new MerchantportallagomappApplication(ctx) with LocalServiceLocator {
 
+      }
+    } { server =>
+      val client = server.serviceClient.implement[MerchantportallagomappService]
+      val merchantRiskScore = MerchantRiskScoreReq.apply("merchantId11", "Medium", "High")
+
+      client.addRiskType.invoke(merchantRiskScore).map { response =>
+        response.merchantId should === ("merchantId11")
+      }
+    }
+  }
+
+  "Get Business Impact for MerchantID" should {
+    "say hello" in ServiceTest.withServer(ServiceTest.defaultSetup.withCluster()) { ctx =>
+      new MerchantportallagomappApplication(ctx) with LocalServiceLocator {
+
+      }
+    } { server =>
+      val client = server.serviceClient.implement[MerchantportallagomappService]
+
+      client.getMerchantImpactData("merchantId123").invoke().map { response =>
+        response.merchantId should === ("merchant123")
+      }
+    }
+  }
 }
